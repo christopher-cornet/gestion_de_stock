0455b216ada25a01e576a70d71fb6e9e12ebeee6
christopher-cornet <115154379+christopher-cornet@users.noreply.github.com>
2023-03-15 16:03:13 +0100

    Start of UI with Tkinter

--- gestion_stock.py
+++ gestion_stock.py
@@ -1,4 +1,6 @@
 import mysql.connector
+from tkinter import * 
+import tkinter as tk
 
 bd = mysql.connector.connect(
     host="localhost",
@@ -8,6 +10,65 @@ bd = mysql.connector.connect(
 )
 
 cursor = bd.cursor()
+
+root = tk.Tk()
+
+root.title("Gestion de stock")
+root.geometry('650x400')
+
+frame = tk.Frame(root)
+frame.pack()
+
+bd_info = tk.LabelFrame(frame, text="Base de données", font=('Arial', 10, 'bold'))
+bd_info.pack(pady=100)
+
+# Texte dans la frame BDD
+# bd_info2 = tk.Label(bd_info, text="Titre")
+# bd_info2.pack()
+
+bd_info3 = tk.LabelFrame(bd_info, text="Produits", fg='green', font=('Arial', 10, 'bold'))
+
+# Infos produits: ID, Nom, description, prix, quantité, categorie
+bd_info2 = tk.Label(bd_info3, text="ID", fg='green')
+bd_info2.grid(row=3, column=0)
+
+bd_info5 = tk.Label(bd_info3, text="Nom", fg='green')
+bd_info5.grid(row=3, column=1)
+
+bd_info6 = tk.Label(bd_info3, text="Description", fg='green')
+bd_info6.grid(row=3, column=2)
+
+bd_info7 = tk.Label(bd_info3, text="Prix", fg='green')
+bd_info7.grid(row=3, column=3)
+
+bd_info8 = tk.Label(bd_info3, text="Quantité", fg='green')
+bd_info8.grid(row=3, column=4)
+
+bd_info9 = tk.Label(bd_info3, text="Catégorie", fg='green')
+bd_info9.grid(row=3, column=5)
+
+bd_info3.pack()
+cursor.execute("SELECT * FROM produit limit 0,10")
+i=0 
+for produit in cursor:
+    for j in range(len(produit)):
+        produit_entry = Entry(bd_info3, width=15, fg='black', justify=CENTER, background="white") 
+        produit_entry.grid(row=i, column=j)
+        produit_entry.insert(END, produit[j]) # Ecrit le nom du produit dans l'entry
+    i=i+1
+
+bd_info4 = tk.LabelFrame(bd_info, text="Catégories", font=('Arial', 10, 'bold'))
+bd_info4.pack()
+cursor.execute("SELECT * FROM categorie limit 0,10")
+i=0 
+for categorie in cursor:
+    for j in range(len(categorie)):
+        categorie_entry = Entry(bd_info4, width=18, fg='black', justify=CENTER) 
+        categorie_entry.grid(row=i, column=j)
+        categorie_entry.insert(END, categorie[j])
+    i=i+1
+
+root.mainloop()
 
 # Créer une base de données nommée “boutique” et les tables “produit” et “categorie”.
 query = "CREATE DATABASE boutique;"
@@ -28,6 +89,8 @@ query6 = "INSERT INTO categorie (nom) VALUES ('Instrument à clavier');"
 query7 = "INSERT INTO categorie (nom) VALUES ('Informatique');"
 
 cursor.execute(query)
+
+bd.commit()
 
 cursor.close()
 
